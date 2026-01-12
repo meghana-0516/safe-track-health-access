99023a585fb5791ba8371485b1cb3f88836e31c4
meghana-0516 <meghanamodukuri05@gmail.com>
2026-01-12 18:28:13 +0530

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()  // Ensure this repository is present
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ExploreSafely"
+include(":app")
+ 
